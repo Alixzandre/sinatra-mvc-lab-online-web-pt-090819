2cc34adda66e206ee5ef24485b605527b6e0c464
Alexander Bispo <alexbispo173@gmail.com>
2019-12-22 04:30:33 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,8 +12,11 @@
      w = words[0]
      words.push(w)
      words.shift
-     if w == 
-     words << "ay"
+     if w == a||e||i||o||uA||E||I||O||U
+       words << "way"
+     else
+      words << "ay"
+    end
      words.join
      
    end
