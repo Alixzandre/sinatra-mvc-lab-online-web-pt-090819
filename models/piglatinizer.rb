6d700354d54faf9a9f934fd48c0d933bb82cc262
Alexander Bispo <alexbispo173@gmail.com>
2019-12-22 04:39:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,17 +8,20 @@
    end"
    
    def piglatinize(text)
+     if text.size > 1
      words = text.split("")
      w = words[0]
      words.push(w)
-    
+     else
+       
+     end
      if w == ('a'||'e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U')
        words << "way"
      else
       words << "ay"
     end
-    words.shift
-     words.join
+      words.shift
+      words.join
      
    end
 end
